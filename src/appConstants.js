11c1e4671e08643bc4e6b7d2b32f5a98d2657c64
Dhani <Dhani@Dhani-2.local>
2019-04-03 16:46:14 +0100

    Use production settings

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,4 +1,4 @@
 export default  {
-  // LOCAL_HOST: 'https://pyaleproperties.herokuapp.com'
-  LOCAL_HOST: 'http://127.0.0.1:8000'
+  LOCAL_HOST: 'https://pyaleproperties.herokuapp.com'
+  // LOCAL_HOST: 'http://127.0.0.1:8000'
 };
